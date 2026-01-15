6da8df38ea2c9822c00c14cb938e97264af82b7d
ZainBarlas0 <zainbarlas0@gmail.com>
2026-01-15 14:18:28 -0500

    Add files via upload

---
+++ src/GameConstants.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class GameConstants {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final int PLAYER_SIZE = 30;
+    public static final int ZOMBIE_SIZE = 30;
+    public static final int BULLET_SIZE = 8;
+    public static final int PLAYER_SPEED = 4;
+    public static final int ZOMBIE_SPEED = 2;
+    public static final int BULLET_SPEED = 10;
+}
